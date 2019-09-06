444792762d263f4007babc96af33b980222184c9
John Klein <johnmbklein@gmail.com>
2019-09-05 17:07:03 -0700

    upated demo code

--- demo.js
+++ demo.js
@@ -1,43 +1,44 @@
 const PlanetNineGateway = require('./planet-nine-gateway.js')
 const crypto = require('planet-nine-crypto')
 
-const keys = crypto.generateKeys('asdf123456')
+const gatewayName = 'npm-plugin-test-gateway6'
 
+const keys = crypto.generateKeys(gatewayName)
+ 
 crypto.getKeys = function() {
   return keys
 }
 
 let gateway = new PlanetNineGateway()
 
-gateway.ongoingGateway({gatewayName: 'npm-plugin-test-gateway3', publicKey: keys.publicKey})
+gateway.ongoingGateway({gatewayName: gatewayName, publicKey: keys.publicKey})
 
-// gateway.getUserIdByUsername('testuser12', (err, userId) => {
+// gateway.getUserIdByUsername('tray-whirl-whirl', (err, userId) => {
 //   if (err) {
 //     console.log(err)
 //   }
-//   gateway.askForOngoingGatewayUsage(userId, (erro, resp) => {
+//   gateway.askForOngoingGatewayUsage(157, (erro, user) => {
 //     if (erro) {
 //       return console.log(erro);
 //     }
-//     console.log('resp', resp);
+//     console.log('user', user);
 //   })
 // })  
 
-gateway.getUser({userId: 157, gatewayName: 'npm-plugin-test-gateway3'}, (err, user) => {
+gateway.getUser({userId: 157, gatewayName: gatewayName}, (err, user) => {
   if (err) console.log(err)
   console.log('user', user)
 }) 
 
-// gateway.getUser({userId: 157, gatewayName: 'npm-plugin-test-gateway3'}, (err, user) => {
+// gateway.getUser({userId: 157, gatewayName: gatewayName}, (err, user) => {
 //   if (err) {
 //     console.log(err)
 //   }
-
+//   console.log('user', user)
 //   const usePowerOpts = {
 //     totalPower: 200,
-//     partnerName: "whirl-five-cool",
-//     userId: user.userId,
-//     ordinal: user.powerOrdinal + 1,
+//     partnerName: "tray-whirl-whirl",
+//     user: user,
 //   }  
 //   gateway.usePowerAtOngoingGateway(usePowerOpts, (err, user) => {
 //     if (err) {
@@ -47,24 +48,28 @@ gateway.getUser({userId: 157, gatewayName: 'npm-plugin-test-gateway3'}, (err, us
 //   })
 // })
 
-gateway.getUser({userId: 157, gatewayName: 'npm-plugin-test-gateway3'}, (err, user) => {
-  if (err) {
-    console.log(err)
-  }
+// gateway.getUser({userId: 157, gatewayName: gatewayName}, (err, user) => {
+//   if (err) {
+//     console.log(err)
+//   }
+//   const requestTransferObj = {
+//     user: user,
+//     destinationUserId: 203,
+//     nineumUniqueIds: ['01000000010204030802020100000002'],
+//   }  
+//   gateway.requestTransfer(requestTransferObj, (err, trasnferRequest) => {
+//     if (err) {
+//       console.log(err)
+//     }
+//     console.log('trasnferRequest', trasnferRequest)
+//   })
+// })
 
-  const requestTransferObj = {
-    userId: user.userId,
-    sourceUserId: user.userId,
-    destinationUserId: 40,
-    nineumUniqueIds: ['01000000010101010505010100000001'],
-    price: 0,
-    currencyName: "USD",
-    ordinal: user.powerOrdinal + 1,
-}  
-  gateway.requestTransfer(requestTransferObj, (err, user) => {
-    if (err) {
-      console.log(err)
-    }
-    console.log('user', user)
-  })
-})
+const nineumHexStringArray = [
+  '01000000010105010203030100000001',
+  '01000000010203090506080100000001',
+  '01000000010203090308020100000001',
+]
+const nineumArray = PlanetNineGateway.getNineumArrayForNineumHexStrings(nineumHexStringArray) 
+
+console.log(typeof nineumArray[0])
